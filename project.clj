3fb05cab96f0d209a7b4bc56da85f749279aa14d
Travis Vachon <travis@copious.com>
2013-12-10 10:45:23 -0500

    Change group so we can maintain our own fork

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure/clojurescript "0.0-SNAPSHOT"
+(defproject com.tvachon/clojurescript "0.0-2080-1"
   :description "ClojureScript compiler and core runtime library"
   :parent [org.clojure/pom.contrib "0.1.2"]
   :url "https://github.com/clojure/clojurescript"
